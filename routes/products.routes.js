9151c913db3175e46e618cab5c73457aa1018dba
FacuBzn <juanfacundobazan@gmail.com>
2023-05-30 17:32:51 -0300

    new validations for product paths with jwt

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,16 +1,36 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validateFields }= require('../middlewares/validate_fields');
+const { validateJWT }= require('../middlewares/validate_jwt');
+
 const { getAllProducts, createProduct, getProductById, deleteProduct, updateProduct } = require('../controller/products.controller');
 
 const router = Router();
 
-router.post('/', createProduct );
+router.post('/', [
+    validateJWT,
+],createProduct );
 
-router.get('/', getAllProducts );
+router.get('/',[
+    validateJWT,
+], getAllProducts );
 
-router.get('/:id', getProductById );
+router.get('/:id',[
+    check('id','It is not a valid id').isMongoId(),
+    validateFields
+], getProductById );
 
-router.put('/:id', updateProduct );
+router.put('/:id',[
+    validateJWT,
+    check('id','It is not a valid id').isMongoId(),
+    validateFields
+], updateProduct );
 
-router.delete('/:id', deleteProduct );
+router.delete('/:id',[
+    validateJWT,
+    check('id','It is not a valid id').isMongoId(),
+    validateFields
+], deleteProduct );
 
 module.exports = router;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validateFields }= require('../middlewares/validate_fields');
+const {validateJWT} = require('../middlewares/validate_jwt');
 
 const { loginUsers, createUsers} = require('../controller/users.controller');
 
@@ -8,6 +9,10 @@ const router = Router();
 
 router.post('/register' , createUsers );
 
-router.post('/login' , loginUsers );
+router.post('/login' ,[
+    check('email', 'the email entered is not valid').isEmail(),
+    check('password', 'the password is required').not().isEmpty(),
+    validateFields
+], loginUsers );
 
 module.exports = router;
